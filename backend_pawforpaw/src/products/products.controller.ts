1a5a7b448c53455ed06a21c7527136bab1ede8f2
Mariana Restrepo <restrepomejiamariana@gmail.com>
2025-06-12 22:11:19 -0500

    Avance CRUD de productos y middleware

--- backend_pawforpaw/src/products/products.controller.ts
+++ backend_pawforpaw/src/products/products.controller.ts
@@ -1,4 +1,65 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { Product } from './product.interface';
+import { CreateProductDto } from './dto/createProduct.dto';
+import { ProductsService } from './products.service';
+import { UpdateProductDto } from './dto/updateProduct.dto';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  //  Obtener lista paginada de productos
+  @Get()
+  getProducts(@Query('page') page?: string, @Query('limit') limit?: string) {
+    const pageNum = Number(page) || 1;
+    const limitNum = Number(limit) || 5;
+    return this.productsService.getProducts(pageNum, limitNum);
+  }
+
+  // Obtener un solo producto por su ID ( GET /products/:id )
+  @Get(':id')
+  findOne(@Param('id') id: string): Product {
+    const product = this.productsService.findOne(id);
+    if (!product) {
+      throw new NotFoundException('Producto no encontrado');
+    }
+    return product;
+  }
+
+  // POST Crear un nuevo producto asociado a un perrito
+  @Post()
+  create(@Body() dto: CreateProductDto): Product {
+    return this.productsService.create(dto);
+  }
+
+  // Actualizar un producto por ID (Put/products/:id)
+  @Put(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateProductDto): Product {
+    try {
+      return this.productsService.update(id, dto);
+    } catch {
+      throw new NotFoundException('Producto no encontrado para actualizar');
+    }
+  }
+
+  // Eliminar un producto por ID (DELETE /products/:id)
+  @Delete(':id')
+  delete(@Param('id') id: string): { id: string } {
+    try {
+      this.productsService.delete(id);
+      return { id };
+    } catch {
+      throw new NotFoundException('PProducto no encontrado para eliminar');
+    }
+  }
+}
--- backend_pawforpaw/src/products/products.service.ts
+++ backend_pawforpaw/src/products/products.service.ts
@@ -1,4 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './product.interface';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateProductDto } from './dto/createProduct.dto';
+import { UpdateProductDto } from './dto/updateProduct.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  //simulo BD temporal
+  private products: Product[] = [];
+
+  create(dto: CreateProductDto): Product {
+    const newProduct: Product = {
+      id: uuidv4(),
+      name: dto.name,
+      price: dto.price,
+      imageUrl: dto.imageUrl,
+      status: true,
+      dogId: dto.dogId,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  getProducts(page: number, limit: number): Product[] {
+    const start = (page - 1) * limit;
+    const end = start + limit;
+    return this.products.slice(start, end);
+  }
+
+  //Obtener todos los productos asociados a un perrito
+  findAllByDog(dogId: number): Product[] {
+    return this.products.filter((product) => product.dogId === dogId);
+  }
+
+  //Obtener un solo producto por ID
+  findOne(id: string): Product | undefined {
+    return this.products.find((product) => product.id === id);
+  }
+
+  //Actualizar un producto por ID
+  update(id: string, dto: UpdateProductDto): Product {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index === -1) {
+      throw new Error('Producto no encontrado');
+    }
+    this.products[index] = { ...this.products[index], ...dto };
+    return this.products[index];
+  }
+
+  // Eliminar un producto por ID
+  delete(id: string): Product {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index === -1) {
+      throw new Error('Producto no encontrado');
+    }
+    const deleted = this.products[index];
+    this.products.splice(index, 1);
+    return deleted;
+  }
+}
