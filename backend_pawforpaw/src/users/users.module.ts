e330bfaee2ff2af8eb167715d7d9b2230721da8a
nicoguaro20 <jungarciagu@unal.edu.co>
2025-06-15 15:35:22 -0500

    fix: ajustes en users.module.ts para integrar nuevos servicios/controladores

--- backend_pawforpaw/src/users/users.module.ts
+++ backend_pawforpaw/src/users/users.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { UserService } from './users.service';
+import { UserRepository } from './users.repository';
+import { UserController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users.entity';
+import { UsersDbService} from './usersDb.service';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService],
+    imports: [TypeOrmModule.forFeature([User])],
+    providers: [
+        UserService,
+        UsersDbService,
+        UserRepository
+    ],
+    controllers: [
+        UserController
+    ],
+    exports: [
+        UserRepository
+    ],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+    };
+};
