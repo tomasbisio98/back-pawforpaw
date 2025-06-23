d894a1898b7874d991db3a987d27b209d058a56c
nicoguaro20 <jungarciagu@unal.edu.co>
2025-06-23 10:24:27 -0500

    Ajuste en auth.services.ts

--- src/auth/auth.services.ts
+++ src/auth/auth.services.ts
@@ -40,7 +40,7 @@ export class AuthService {
     const token = this.jwtService.sign(userPayload);
 
     return {
-      accessToken: token,
+      Token: token,
       "user": {
         id: user.id,
         name: user.name,
@@ -109,7 +109,7 @@ export class AuthService {
     const token = this.jwtService.sign(userPayload);
 
     return {
-      accessToken: token,
+      Token: token,  //ajuste menor para forzar el commit
       "user": {
         id: user.id,
         name: user.name,
