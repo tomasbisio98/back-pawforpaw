d111d2c20ea735463a66cfe6807634598143b493
nicoguaro20 <jungarciagu@unal.edu.co>
2025-06-25 17:51:52 -0500

    Cambios para el solicitud de tipo PUT

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,9 +1,9 @@
 import {
-  IsEmail,
   IsOptional,
   IsString,
   Length,
   IsPhoneNumber,
+  IsBoolean,
 } from 'class-validator';
 
 export class UpdateUserDto {
@@ -13,34 +13,9 @@ export class UpdateUserDto {
   name?: string;
 
   @IsOptional()
-  @IsEmail()
-  email?: string;
+  phone?: string;
 
   @IsOptional()
-  @IsString()
-  @Length(8, 20)
-  password?: string;
-
-  @IsOptional()
-  @IsString()
-  confirmPassword?: string;
-
-  @IsOptional()
-  @IsString()
-  @Length(3, 80)
-  country?: string;
-
-  @IsOptional()
-  @IsPhoneNumber()
-  phone?: number;
-
-  @IsOptional()
-  @IsString()
-  @Length(3, 80)
-  address?: string;
-
-  @IsOptional()
-  @IsString()
-  @Length(3, 20)
-  city?: string;
+  @IsBoolean()
+  status?: boolean;
 }
