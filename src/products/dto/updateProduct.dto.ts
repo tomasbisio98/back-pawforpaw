f37a5697681513d1ab56d1f91bcde0081047259f
Nicolas Gonzalez <nicolasxd48@gmail.com>
2025-06-26 18:33:33 -0500

    Cambio en Products Dto

--- src/products/dto/updateProduct.dto.ts
+++ src/products/dto/updateProduct.dto.ts
@@ -1,5 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class UpdateProductDto {
   @ApiProperty({
@@ -31,5 +37,6 @@ export class UpdateProductDto {
     example: true,
   })
   @IsOptional()
+  @IsBoolean()
   status?: boolean;
 }
