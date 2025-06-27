8d924fe14d6df5aac265a11511c0b0a16a28c4eb
Tomás Bisio <tomibisio1@gmail.com>
2025-06-26 22:57:55 -0300

    adding more seeders: newsletter / productsForDogs (#71)

--- src/seeder/seeder.module.ts
+++ src/seeder/seeder.module.ts
@@ -4,11 +4,21 @@ import { SeedService } from './seeder.service';
 import { Dog } from 'src/dogs/entities/dog.entity';
 import { Products } from 'src/products/entities/products.entity';
 import { User } from 'src/users/users.entity';
+import { NewsletterSubscription } from '../newsletter/entities/subscription.entity'; // 👈 nuevo import
 import { UserAdminSeeder } from './user-admin.seeder';
 import { UsersModule } from 'src/users/users.module';
+import { DogProductSeeder } from './dog-product.seeder'; // 👈 seeder de relación perro-producto
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Dog, Products, User]), UsersModule],
-  providers: [SeedService, UserAdminSeeder],
+  imports: [
+    TypeOrmModule.forFeature([
+      Dog,
+      Products,
+      User,
+      NewsletterSubscription, // 👈 agregado aquí
+    ]),
+    UsersModule,
+  ],
+  providers: [SeedService, UserAdminSeeder, DogProductSeeder],
 })
 export class SeedModule {}
--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -1,22 +1,32 @@
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Dog } from 'src/dogs/entities/dog.entity';
-import { Products } from 'src/products/entities/products.entity';
+import { Dog } from '../dogs/entities/dog.entity';
+import { Products } from '../products/entities/products.entity';
 import { Repository } from 'typeorm';
-import { dogs, products, users } from './seeder';
-import { User } from 'src/users/users.entity';
+import { users } from './users.seeder';
+import { User } from '../users/users.entity';
 import { UserAdminSeeder } from './user-admin.seeder';
+import * as bcrypt from 'bcrypt';
+import { DogProductSeeder } from './dog-product.seeder';
+import { NewsletterSubscription } from 'src/newsletter/entities/subscription.entity';
 
 @Injectable()
 export class SeedService implements OnApplicationBootstrap {
   constructor(
     @InjectRepository(Dog)
     private readonly dogRepository: Repository<Dog>,
+
     @InjectRepository(Products)
     private readonly productRepository: Repository<Products>,
+
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+
+    @InjectRepository(NewsletterSubscription)
+    private readonly newsletterRepository: Repository<NewsletterSubscription>,
+
     private readonly userAdminSeeder: UserAdminSeeder,
+    private readonly dogProductSeeder: DogProductSeeder,
   ) {}
 
   async onApplicationBootstrap() {
@@ -25,20 +35,41 @@ export class SeedService implements OnApplicationBootstrap {
     const usersCount = await this.userRepository.count();
 
     if (dogsCount === 0) {
+      // Make sure the path and export are correct
+      const { dogs } = await import('./dogs.seeder');
       await this.dogRepository.save(dogs);
       console.log('🐶 Seed: 15 dogs insertados');
     }
 
     if (productsCount === 0) {
+      const { products } = await import('./products.seeder');
       await this.productRepository.save(products);
       console.log('📦 Seed: 15 productos insertados');
     }
 
     if (usersCount === 0) {
-      await this.userRepository.save(users);
-      console.log('👩🏻‍💻 Seed: 15 usuarios insertados');
+      const hashedUsers = await Promise.all(
+        users.map(async (user) => ({
+          ...user,
+          password: await bcrypt.hash(user.password ?? '', 10),
+        })),
+      );
+
+      await this.userRepository.save(hashedUsers);
+      console.log('👩🏻‍💻 Seed: 15 usuarios con contraseñas hasheadas insertados');
     }
 
     await this.userAdminSeeder.run();
+    await this.dogProductSeeder.run();
+
+    const newsletterCount = await this.newsletterRepository.count();
+
+    if (newsletterCount === 0) {
+      const { newsletterSubscribers } = await import(
+        './newsletter-subscribers.seeder'
+      );
+      await this.newsletterRepository.save(newsletterSubscribers);
+      console.log('📧 Seed: 6 suscriptores insertados');
+    }
   }
 }
