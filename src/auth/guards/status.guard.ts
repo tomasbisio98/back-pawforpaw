6cd3efb241266bad1adca1b042439bbde173c55f
nicoguaro20 <jungarciagu@unal.edu.co>
2025-06-29 17:54:49 -0500

    Ajustes guardian status

--- src/auth/guards/status.guard.ts
+++ src/auth/guards/status.guard.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Reflector } from '@nestjs/core';
-import { UserRepository } from 'src/users/users.repository';
+import { UserRepository } from 'src/users/users.repository'; // comentario para hacer nuevamente commit
 
 @Injectable()
 export class StatusGuard implements CanActivate {
