02db20a38856bccee139c1ddbe344e1314ba68b1
Mariana Restrepo <restrepomejiamariana@gmail.com>
2025-06-29 19:25:51 -0500

    exitoso

--- src/products/dto/updateProduct.dto.ts
+++ src/products/dto/updateProduct.dto.ts
@@ -33,7 +33,7 @@ export class UpdateProductDto {
   imageUrl?: string;
 
   @ApiProperty({
-    description: 'true || fale',
+    description: 'true || false',
     example: true,
   })
   @IsOptional()
